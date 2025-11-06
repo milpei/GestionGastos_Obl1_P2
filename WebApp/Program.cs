a835f1b6664e1f1ebe7ebfa5b2ad94f9dc506213
Gabriel Mendez <gabrielmendezgrassi@gmail.com>
2025-11-06 10:56:01 -0300

    Ahora carga, vamos por partes

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -31,7 +31,7 @@ namespace WebApp
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Login}/{action=Login}/{id?}");
+                pattern: "{controller=Login}/{action=Index}/{id?}");
 
             app.Run();
         }
